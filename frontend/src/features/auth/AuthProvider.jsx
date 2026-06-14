b2733da1e799844e38d31b5a79374fa60f655b5b
kokoab <batiancelafrancis747@gmail.com>
2026-06-14 14:18:43 +0800

    removed unnecessary import

--- frontend/src/features/auth/AuthProvider.jsx
+++ frontend/src/features/auth/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 import { supabase } from '../../lib/supabase';
 
 const AuthContext = createContext(null);
